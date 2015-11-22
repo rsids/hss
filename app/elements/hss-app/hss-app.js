40a286541142424872ecf412f6642c71aaccd596
Ids Klijnsma <git@idsklijnsma.nl>
2015-11-22 21:27:57 +0100

    App now correctly transpiles to es5

---
+++ app/elements/hss-app/hss-app.js
@@ -0,0 +1,129 @@
+'use strict';
+
+class HssApp {
+
+    attached() {
+        addEventListener('paper-header-transform', (e) => {
+            var appName = document.querySelector('#mainToolbar .app-name');
+            var middleContainer = document.querySelector('#mainToolbar .middle-container');
+            var bottomContainer = document.querySelector('#mainToolbar .bottom-container');
+            var detail = e.detail;
+            var heightDiff = detail.height - detail.condensedHeight;
+            var yRatio = Math.min(1, detail.y / heightDiff);
+            // appName max size when condensed.
+            // The smaller the number the smaller the condensed size.
+            var maxMiddleScale = 0.50;
+            var scaleMiddle = Math.max(
+                maxMiddleScale,
+                (heightDiff - detail.y) / (heightDiff / (1 - maxMiddleScale))  + maxMiddleScale);
+            var scaleBottom = 1 - yRatio;
+
+            // Move/translate middleContainer
+            Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
+
+            // Scale bottomContainer and bottom sub title to nothing and back
+            Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
+
+            // Scale middleContainer appName
+            Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
+        });
+
+    }
+
+    beforeRegister() {
+        this.is = 'hss-app';
+        this.isConnected = false;
+        this.connectionIcon = 'hardware:cast';
+        this.channels = {};
+    }
+
+    connect() {
+        if(this.clientId) {
+
+            let action = this.isConnected ? 'disconnect' : 'registerServer';
+            this.socket.send(JSON.stringify({action: action}));
+            this.isConnected = !this.isConnected;
+            this.connectionIcon = this.isConnected ? 'hardware:cast-connected' : 'hardware:cast';
+        }
+    }
+
+    reconnect() {
+        if(this.serverUrl) {
+            this.loadSocket();
+        }
+    }
+
+    onPlay(e) {
+
+        this.socket.send(JSON.stringify({
+            action: 'playSound',
+            data: e.detail.sound,
+            clientId: this.clientId
+        }));
+    }
+
+    onKeyUp(e) {
+        if(e.which === 13) {
+            this.fire('hss-submit-search', {query: e.currentTarget.value});
+        }
+    }
+
+    handleResponse() {
+        if(this.ajaxResponse) {
+            this.serverUrl = `http://${this.ajaxResponse.server}/hss/`;
+            this.socketUrl  = `ws://${this.ajaxResponse.server}:${this.ajaxResponse.port}/socket/hssSocket.php`;
+            this.dataUrl = this.serverUrl + 'getSounds.php';
+        }
+        this.loadSocket();
+    }
+
+    loadSocket() {
+        if(this.socket) {
+            this.socket.close();
+        }
+
+        try {
+            this.socket = new WebSocket(this.socketUrl);
+            this.socket.onopen = (msg) => {
+                this.socket.send(JSON.stringify({action: 'registerClient'}));
+                console.log('Socket opened ', msg);
+            };
+
+            this.socket.onmessage = (msg) => {
+                let data = JSON.parse(msg.data);
+
+                switch(data.msg) {
+                    case 'clientRegistered':
+                        this.clientId = data.data;
+                        this.connect();
+                        break;
+                    case 'playSound':
+                        this.playSound(data.data, data.clientId);
+                        break;
+
+                }
+            };
+
+        } catch (ex) {
+            console.log('ex', ex);
+        }
+    }
+
+    playSound(file, channel) {
+        if(!this.channels.hasOwnProperty(channel)) {
+            this.channels[channel] = new Audio();
+        }
+
+        this.channels[channel].src = this.serverUrl + file;
+        this.channels[channel].volume = 1;
+        this.channels[channel].play();
+
+        if(channel !== this.clientId) {
+            // If another client wants to play a sound,
+            // show the ripple effect
+            this.fire('hss-play-sound', {sound: file});
+        }
+    }
+
+}
+Polymer(HssApp);
--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,11 @@ var reload = browserSync.reload;
 var merge = require('merge-stream');
 var path = require('path');
 var fs = require('fs');
-var glob = require('glob');
+var glob = require('glob-all');
 var historyApiFallback = require('connect-history-api-fallback');
 var packageJson = require('./package.json');
 var crypto = require('crypto');
+// var ghPages = require('gulp-gh-pages');
 
 var AUTOPREFIXER_BROWSERS = [
     'ie >= 10',
@@ -36,7 +37,13 @@ var AUTOPREFIXER_BROWSERS = [
     'bb >= 10'
 ];
 
-var styleTask = function (stylesPath, srcs) {
+var DIST = 'dist';
+
+var dist = function(subpath) {
+    return !subpath ? DIST : path.join(DIST, subpath);
+};
+
+var styleTask = function(stylesPath, srcs) {
     return gulp.src(srcs.map(function(src) {
         return path.join('app', stylesPath, src);
     }))
@@ -44,37 +51,31 @@ var styleTask = function (stylesPath, srcs) {
         .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
         .pipe(gulp.dest('.tmp/' + stylesPath))
         .pipe($.cssmin())
-        .pipe(gulp.dest('dist/' + stylesPath))
+        .pipe(gulp.dest(dist(stylesPath)))
         .pipe($.size({title: stylesPath}));
 };
 
-var jshintTask = function (src) {
+var imageOptimizeTask = function(src, dest) {
     return gulp.src(src)
-        .pipe($.jshint.extract()) // Extract JS from .html files
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'))
-        .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
-};
-
-var imageOptimizeTask = function (src, dest) {
-    return gulp.src(src)
-        .pipe($.cache($.imagemin({
+        .pipe($.imagemin({
             progressive: true,
             interlaced: true
-        })))
+        }))
         .pipe(gulp.dest(dest))
         .pipe($.size({title: 'images'}));
 };
 
-var optimizeHtmlTask = function (src, dest) {
-    var assets = $.useref.assets({searchPath: ['.tmp', 'app', 'dist']});
+var optimizeHtmlTask = function(src, dest) {
+    var assets = $.useref.assets();
 
     return gulp.src(src)
         // Replace path for vulcanized assets
         .pipe($.if('*.html', $.replace('elements/elements.html', 'elements/elements.vulcanized.html')))
         .pipe(assets)
         // Concatenate and minify JavaScript
-        .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
+        .pipe($.if('*.js', $.uglify({
+            preserveComments: 'some'
+        })))
         // Concatenate and minify styles
         // In case you are still using useref build blocks
         .pipe($.if('*.css', $.cssmin()))
@@ -88,96 +89,109 @@ var optimizeHtmlTask = function (src, dest) {
         })))
         // Output files
         .pipe(gulp.dest(dest))
-        .pipe($.size({title: 'html'}));
+        .pipe($.size({
+            title: 'html'
+        }));
 };
 
 // Compile and automatically prefix stylesheets
-gulp.task('styles', function () {
+gulp.task('styles', function() {
     return styleTask('styles', ['**/*.css']);
 });
 
-gulp.task('elements', function () {
+gulp.task('elements', function() {
     return styleTask('elements', ['**/*.css']);
 });
 
 // Lint JavaScript
-gulp.task('jshint', function () {
-    return jshintTask([
+gulp.task('lint', function() {
+    return gulp.src([
         'app/scripts/**/*.js',
         'app/elements/**/*.js',
         'app/elements/**/*.html',
         'gulpfile.js'
     ])
-        .pipe($.jshint.extract()) // Extract JS from .html files
+        .pipe(reload({
+            stream: true,
+            once: true
+        }))
+
+        // JSCS has not yet a extract option
+        .pipe($.if('*.html', $.htmlExtract()))
         .pipe($.jshint())
+        .pipe($.jscs())
+        .pipe($.jscsStylish.combineWithHintResults())
         .pipe($.jshint.reporter('jshint-stylish'))
         .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
 // Optimize images
-gulp.task('images', function () {
-    return imageOptimizeTask('app/images/**/*', 'dist/images');
+gulp.task('images', function() {
+    return imageOptimizeTask('app/images/**/*', dist('images'));
 });
 
 // Copy all files at the root level (app)
-gulp.task('copy', function () {
+gulp.task('copy', function() {
     var app = gulp.src([
         'app/*',
         '!app/test',
         '!app/cache-config.json'
     ], {
         dot: true
-    }).pipe(gulp.dest('dist'));
+    }).pipe(gulp.dest(dist()));
 
     var bower = gulp.src([
         'bower_components/**/*'
-    ]).pipe(gulp.dest('dist/bower_components'));
+    ]).pipe(gulp.dest(dist('bower_components')));
 
     var elements = gulp.src(['app/elements/**/*.html',
         'app/elements/**/*.css',
-        'app/elements/**/*.js'])
-        .pipe(gulp.dest('dist/elements'));
+        'app/elements/**/*.js'
+    ])
+        .pipe(gulp.dest(dist('elements')));
 
     var swBootstrap = gulp.src(['bower_components/platinum-sw/bootstrap/*.js'])
-        .pipe(gulp.dest('dist/elements/bootstrap'));
+        .pipe(gulp.dest(dist('elements/bootstrap')));
 
     var swToolbox = gulp.src(['bower_components/sw-toolbox/*.js'])
-        .pipe(gulp.dest('dist/sw-toolbox'));
+        .pipe(gulp.dest(dist('sw-toolbox')));
 
     var vulcanized = gulp.src(['app/elements/elements.html'])
         .pipe($.rename('elements.vulcanized.html'))
-        .pipe(gulp.dest('dist/elements'));
+        .pipe(gulp.dest(dist('elements')));
 
-    var hss = gulp.src(['app/hss/**/*.*'])
-        .pipe(gulp.dest('dist/hss'));
-
-    return merge(app, bower, elements, vulcanized, swBootstrap, swToolbox, hss)
-        .pipe($.size({title: 'copy'}));
+    return merge(app, bower, elements, vulcanized, swBootstrap, swToolbox)
+        .pipe($.size({
+            title: 'copy'
+        }));
 });
 
 // Copy web fonts to dist
-gulp.task('fonts', function () {
+gulp.task('fonts', function() {
     return gulp.src(['app/fonts/**'])
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe($.size({title: 'fonts'}));
+        .pipe(gulp.dest(dist('fonts')))
+        .pipe($.size({
+            title: 'fonts'
+        }));
 });
 
 // Scan your HTML for assets & optimize them
-gulp.task('html', function () {
+gulp.task('html', function() {
     return optimizeHtmlTask(
-        ['app/**/*.html', '!app/{elements,test}/**/*.html'],
-        'dist');
+        [dist('/**/*.html'), '!' + dist('/{elements,test}/**/*.html')],
+        dist());
 });
 
 // Vulcanize granular configuration
-gulp.task('vulcanize', function () {
-    var DEST_DIR = 'dist/elements';
-    return gulp.src('dist/elements/elements.vulcanized.html')
+gulp.task('vulcanize', function() {
+    var DEST_DIR = dist('elements');
+    return gulp.src(dist('elements/elements.vulcanized.html'))
         .pipe($.vulcanize({
             stripComments: true,
             inlineCss: true,
             inlineScripts: true
         }))
+        .pipe($.minifyInline())
         .pipe(gulp.dest(DEST_DIR))
         .pipe($.size({title: 'vulcanize'}));
 });
@@ -189,45 +203,49 @@ gulp.task('vulcanize', function () {
 // in your project, please enable it within the 'default' task.
 // See https://github.com/PolymerElements/polymer-starter-kit#enable-service-worker-support
 // for more context.
-gulp.task('cache-config', function (callback) {
-    var dir = 'dist';
+gulp.task('cache-config', function(callback) {
+    var dir = dist();
     var config = {
         cacheId: packageJson.name || path.basename(__dirname),
         disabled: false
     };
 
-    glob('{elements,scripts,styles}/**/*.*', {cwd: dir}, function(error, files) {
-        if (error) {
-            callback(error);
-        } else {
-            files.push('index.html', './', 'bower_components/webcomponentsjs/webcomponents-lite.min.js');
-            config.precache = files;
+    glob([
+            'index.html',
+            './',
+            'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+            '{elements,scripts,styles}/**/*.*'],
+        {cwd: dir}, function(error, files) {
+            if (error) {
+                callback(error);
+            } else {
+                config.precache = files;
 
-            var md5 = crypto.createHash('md5');
-            md5.update(JSON.stringify(config.precache));
-            config.precacheFingerprint = md5.digest('hex');
+                var md5 = crypto.createHash('md5');
+                md5.update(JSON.stringify(config.precache));
+                config.precacheFingerprint = md5.digest('hex');
 
-            var configPath = path.join(dir, 'cache-config.json');
-            fs.writeFile(configPath, JSON.stringify(config), callback);
-        }
-    });
+                var configPath = path.join(dir, 'cache-config.json');
+                fs.writeFile(configPath, JSON.stringify(config), callback);
+            }
+        });
 });
 
 // Clean output directory
-gulp.task('clean', function (cb) {
-    del(['.tmp', 'dist'], cb);
+gulp.task('clean', function() {
+    return del(['.tmp', dist()]);
 });
 
 // Watch files for changes & reload
-gulp.task('serve', ['styles', 'elements', 'images'], function () {
-    browserSync({
+gulp.task('serve', ['lint', 'styles', 'elements', 'images', 'js'], function() {
+browserSync({
         port: 5000,
         notify: false,
         logPrefix: 'PSK',
         snippetOptions: {
             rule: {
                 match: '<span id="browser-sync-binding"></span>',
-                fn: function (snippet) {
+                fn: function(snippet) {
                     return snippet;
                 }
             }
@@ -238,22 +256,22 @@ gulp.task('serve', ['styles', 'elements', 'images'], function () {
         // https: true,
         server: {
             baseDir: ['.tmp', 'app'],
-            middleware: [ historyApiFallback() ],
+            middleware: [historyApiFallback()],
             routes: {
                 '/bower_components': 'bower_components'
             }
         }
     });
 
-    gulp.watch(['app/**/*.html'], reload);
+    gulp.watch(['app/**/*.html'], ['js', reload]);
     gulp.watch(['app/styles/**/*.css'], ['styles', reload]);
     gulp.watch(['app/elements/**/*.css'], ['elements', reload]);
-    gulp.watch(['app/{scripts,elements}/**/{*.js,*.html}'], ['jshint']);
+    gulp.watch(['app/{scripts,elements}/**/{*.js,*.html}'], ['lint', 'js']);
     gulp.watch(['app/images/**/*'], reload);
 });
 
 // Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], function () {
+gulp.task('serve:dist', ['default'], function() {
     browserSync({
         port: 5001,
         notify: false,
@@ -261,7 +279,7 @@ gulp.task('serve:dist', ['default'], function () {
         snippetOptions: {
             rule: {
                 match: '<span id="browser-sync-binding"></span>',
-                fn: function (snippet) {
+                fn: function(snippet) {
                     return snippet;
                 }
             }
@@ -270,20 +288,47 @@ gulp.task('serve:dist', ['default'], function () {
         // Note: this uses an unsigned certificate which on first access
         //       will present a certificate warning in the browser.
         // https: true,
-        server: 'dist',
-        middleware: [ historyApiFallback() ]
+        server: dist(),
+        middleware: [historyApiFallback()]
     });
 });
 
 // Build production files, the default task
-gulp.task('default', ['clean'], function (cb) {
+gulp.task('default', ['clean'], function(cb) {
     // Uncomment 'cache-config' if you are going to use service workers.
     runSequence(
         ['copy', 'styles'],
-        'elements',
-        ['jshint', 'images', 'fonts', 'html'],
+        ['elements', 'js'],
+        ['lint', 'images', 'fonts', 'html'],
         'vulcanize', // 'cache-config',
         cb);
+});
+
+// Build then deploy to GitHub pages gh-pages branch
+gulp.task('build-deploy-gh-pages', function(cb) {
+    runSequence(
+        'default',
+        'deploy-gh-pages',
+        cb);
+});
+
+// Deploy to GitHub pages gh-pages branch
+gulp.task('deploy-gh-pages', function() {
+    return gulp.src(dist('**/*'))
+        .pipe($.ghPages());
+});
+
+// Transpile all JS to ES5.
+gulp.task('js', function() {
+    return gulp.src(['app/**/*.{js,html}'])
+        .pipe($.sourcemaps.init())
+        .pipe($.if('*.html', $.crisper())) // Extract JS from .html files
+        .pipe($.if('*.js', $.babel({
+            presets: ['es2015']
+        })))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('.tmp/'))
+        .pipe(gulp.dest(dist()));
 });
 
 // Load tasks for web-component-tester
@@ -291,4 +336,6 @@ gulp.task('default', ['clean'], function (cb) {
 require('web-component-tester').gulp.init(gulp);
 
 // Load custom tasks from the `tasks` directory
-try { require('require-dir')('tasks'); } catch (err) {}
+try {
+    require('require-dir')('tasks');
+} catch (err) {}
